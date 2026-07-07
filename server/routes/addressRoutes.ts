a0e1ed6684b04fb8a8c8cba988d9ccfdeae86bd8
Dilakshan Madhusanka <madhusankamrd@gmail.com>
2026-07-07 11:49:34 +0530

    backend completed

---
+++ server/routes/addressRoutes.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import auth from "../middleware/auth.js";
+import { addAddress, deleteAddress, getAddresses, updateAddress } from "../controllers/addressController.js";
+
+const addressRouter = express.Router();
+
+addressRouter.get("/", auth, getAddresses);
+addressRouter.post("/", auth, addAddress);
+addressRouter.get("/:id", auth, updateAddress);
+addressRouter.get("/:id", auth, deleteAddress);
+
+export default addressRouter;
